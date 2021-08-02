efc36d51ae8cd22c43a04388591a72a5babcaccf
Alvin Mukiibi <alvin.mukiibi@umeme.co.ug>
2021-08-02 22:48:08 +0300

    added vue js, favicon, readme content, controller logic and other scaffolding

---
+++ resources/js/views/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+     <h1 class="text-primary font-weight-bold">{{message}}</h1>
+  </div>
+</template>
+<script>
+const default_layout = "default";
+
+
+export default {
+  computed: {},
+  data() {
+      return {
+          message:'Welcome to The Century Ebooks Library Website!!'
+      }
+  }
+};
+</script>
