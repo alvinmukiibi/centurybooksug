b68cff33370ac29606f57b2645a72b973158cee6
AustinStan <austinstan24@gmail.com>
2021-08-30 09:08:02 +0300

    Update Home.vue

--- resources/js/components/Pages/Home.vue
+++ resources/js/components/Pages/Home.vue
@@ -1,94 +1,207 @@
 <template>
     <div>
-    <br>
+        <br />
         <section class="banner3">
             <div class="container">
                 <div class="row">
                     <div class="col-md-2">
                         <div class="banner3-box">
-                            <a href="#"><img src="century-images/7.jpg" alt="" class="img-fluid"></a>
+                            <a href="#"
+                                ><img
+                                    src="century-images/7.jpg"
+                                    alt=""
+                                    class="img-fluid"
+                            /></a>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="banner3-box">
-                            <a href="#"><img src="century-images/8.jpeg" alt="" class="img-fluid"></a>
+                            <a href="#"
+                                ><img
+                                    src="century-images/8.jpeg"
+                                    alt=""
+                                    class="img-fluid"
+                            /></a>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="banner3-box">
-                            <a href="#"><img src="century-images/9.jpg" alt="" class="img-fluid"></a>
+                            <a href="#"
+                                ><img
+                                    src="century-images/9.jpg"
+                                    alt=""
+                                    class="img-fluid"
+                            /></a>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="banner3-box">
-                            <a href="#"><img src="century-images/10.jpg" alt="" class="img-fluid"></a>
+                            <a href="#"
+                                ><img
+                                    src="century-images/10.jpg"
+                                    alt=""
+                                    class="img-fluid"
+                            /></a>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="banner3-box">
-                            <a href="#"><img src="century-images/11.jpg" alt="" class="img-fluid"></a>
+                            <a href="#"
+                                ><img
+                                    src="century-images/11.jpg"
+                                    alt=""
+                                    class="img-fluid"
+                            /></a>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="banner3-box">
-                            <a href="#"><img src="century-images/12.jpg" alt="" class="img-fluid"></a>
+                            <a href="#"
+                                ><img
+                                    src="century-images/12.jpg"
+                                    alt=""
+                                    class="img-fluid"
+                            /></a>
                         </div>
                     </div>
-                    
-                   
                 </div>
-                <br>
+                <br />
                 <div class="row">
                     <div class="col-md-2">
                         <div class="banner3-box">
-                            <a href="#"><img src="century-images/1.jpg" alt="" class="img-fluid"></a>
+                            <a href="#"
+                                ><img
+                                    src="century-images/1.jpg"
+                                    alt=""
+                                    class="img-fluid"
+                            /></a>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="banner3-box">
-                            <a href="#"><img src="century-images/2.jpg" alt="" class="img-fluid"></a>
+                            <a href="#"
+                                ><img
+                                    src="century-images/2.jpg"
+                                    alt=""
+                                    class="img-fluid"
+                            /></a>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="banner3-box">
-                            <a href="#"><img src="century-images/3.jpg" alt="" class="img-fluid"></a>
+                            <a href="#"
+                                ><img
+                                    src="century-images/3.jpg"
+                                    alt=""
+                                    class="img-fluid"
+                            /></a>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="banner3-box">
-                            <a href="#"><img src="century-images/4.jpg" alt="" class="img-fluid"></a>
+                            <a href="#"
+                                ><img
+                                    src="century-images/4.jpg"
+                                    alt=""
+                                    class="img-fluid"
+                            /></a>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="banner3-box">
-                            <a href="#"><img src="century-images/5.jpg" alt="" class="img-fluid"></a>
+                            <a href="#"
+                                ><img
+                                    src="century-images/5.jpg"
+                                    alt=""
+                                    class="img-fluid"
+                            /></a>
                         </div>
                     </div>
                     <div class="col-md-2">
                         <div class="banner3-box">
-                            <a href="#"><img src="century-images/6.jpg" alt="" class="img-fluid"></a>
+                            <a href="#"
+                                ><img
+                                    src="century-images/6.jpg"
+                                    alt=""
+                                    class="img-fluid"
+                            /></a>
                         </div>
                     </div>
-                    
-                   
                 </div>
+                    <!-- <div class="row " style="margin-top:50px;">
+                        <div class="col-md-12">
+                            <div class="tp-bnd owl-carousel">
+                                <div class="bnd-items">
+                                    <a href="#"
+                                        ><img
+                                            src="images/brand-01.png"
+                                            alt=""
+                                            class="img-fluid"
+                                    /></a>
+                                </div>
+                                <div class="bnd-items">
+                                    <a href="#"
+                                        ><img
+                                            src="images/brand-02.png"
+                                            alt=""
+                                            class="img-fluid"
+                                    /></a>
+                                </div>
+                                <div class="bnd-items">
+                                    <a href="#"
+                                        ><img
+                                            src="images/brand-03.png"
+                                            alt=""
+                                            class="img-fluid"
+                                    /></a>
+                                </div>
+                                <div class="bnd-items">
+                                    <a href="#"
+                                        ><img
+                                            src="images/brand-04.png"
+                                            alt=""
+                                            class="img-fluid"
+                                    /></a>
+                                </div>
+                                <div class="bnd-items">
+                                    <a href="#"
+                                        ><img
+                                            src="images/brand-05.png"
+                                            alt=""
+                                            class="img-fluid"
+                                    /></a>
+                                </div>
+                                <div class="bnd-items">
+                                    <a href="#"
+                                        ><img
+                                            src="images/brand-06.png"
+                                            alt=""
+                                            class="img-fluid"
+                                    /></a>
+                                </div>
+                                <div class="bnd-items">
+                                    <a href="#"
+                                        ><img
+                                            src="images/brand-07.png"
+                                            alt=""
+                                            class="img-fluid"
+                                    /></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
             </div>
         </section>
-       
     </div>
 </template>
 
 <script>
 export default {
-      name: 'Home',
+    name: "Home",
     components: {},
     data() {
-        return {
-            
-        }
+        return {};
     },
-    methods: {
-        
-    },
-}
+    methods: {}
+};
 </script>
